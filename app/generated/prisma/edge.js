fcec055d1d128043763540036a3e3ac25770a165
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2025-04-19 01:56:32 +0530

    update package.json

--- app/generated/prisma/edge.js
+++ app/generated/prisma/edge.js
@@ -204,7 +204,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
